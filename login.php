6f947bdef12bea0faf94e7b769ed9e988982a95b
Yanis <135709548+oni00@users.noreply.github.com>
2026-03-11 11:24:36 +0100

    Implement user login with password verification

--- login.php
+++ login.php
@@ -1,17 +1,38 @@
 
 <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])) {
     print_r($_POST);
     $email = $_POST["email"];
-    $mot_de_passe = $_POST["mot_de_passe"];
+    $password_hash = $_POST["mot_de_passe"];
 
-    if (empty($email) || empty ($mot_de_passe)){
+    if (empty($email) || empty ($password_hash)){
         echo "veuilez remplir tous les champs";
     }
 
 
     require "db.php";
     
+    $selectUser = "select * from utilisateur where email = ?";
+    $stmt = $connect->prepare($selectUser);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $userInfo= $result->fetch_assoc();
+
+    if (password_verify($password_hash, $userInfo["passw"])){
+
+        session_start();
+        $_SESSION["user_id"]=$userInfo["id"];
+        $_SESSION["email"]=$userInfo["email"];
+        $_SESSION["role"]=$userInfo["role"];
+        
+        header("location: index.php");
+        exit;
+
+    }else{
+
+        echo "mot de passe ou email incorrect";
+    }
     
     
     }
@@ -35,11 +56,10 @@
     <input type="email" name="email">
     <label for="mot de passe">mot de passe</label>
     <input type="password" name="mot_de_passe">
-    <button type="submit" name="login">login</button>
-  
+    <button type="submit" name="login">connectez-vous</button>
+     <a href="inscription.php">Pas encore inscrit ?</a>
     
     </form>
 
 </body>
-
 </html>
