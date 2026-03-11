d4b81caac23ff912be8d13c26523c15c3f8e0e5e
bbrp5 <159815822+bbrp5@users.noreply.github.com>
2026-03-11 11:30:41 +0100

    Fix formatting and improve PHP registration script

--- Inscripton.php
+++ Inscripton.php
@@ -17,7 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["inscription"])) {
         $mdphash = password_hash($mot_de_passe, PASSWORD_DEFAULT);
 
         $insert = "insert into utilisateur (nom, prenom, email, mot_de_passe, role) values (?,?,?,?,?)";
-        $req = $connect->prepare($insert);
+        $req = $connexion->prepare($insert);
         $req->bind_param("sssss", $nom, $prenom, $email, $mdphash, $role );
         $req->execute(); 
         header("Location: login.php");
@@ -54,4 +54,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["inscription"])) {
         <button type="submit" name="inscription">S'inscrire</button>
     </form>
 </body>
+
 </html>
