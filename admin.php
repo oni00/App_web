562c6d30a11e4fba18ae1a6056a259f702fa5a12
bbrp5 <159815822+bbrp5@users.noreply.github.com>
2026-03-11 11:16:52 +0100

    Add status field to user insertion in admin.php

--- admin.php
+++ admin.php
@@ -5,18 +5,19 @@ $prenom = 'admin';
 $email = 'test@test.com';
 $mot_de_passe = 'TestEncore';
 $role = 'administrateur';
+$status = 'actif';
 
-require "db.php";
+require "../config/db.php";
 
 $mdphash = password_hash($mot_de_passe, PASSWORD_DEFAULT);
 
 
-$insert = "insert into utilisateur (nom, prenom, email, mot_de_passe, role) values (?,?,?,?,?)";
+$insert = "insert into utilisateur (nom, email, password_hash, role,statut) values (?,?,?,?,?)";
 $req = $connexion->prepare($insert);
-$req->bind_param("sssss", $nom, $prenom, $email, $mdphash, $role );
+$req->bind_param("sssss", $nom, $email, $mdphash, $role, $status);
 $req->execute(); 
 
 
 
-
 ?>
+
